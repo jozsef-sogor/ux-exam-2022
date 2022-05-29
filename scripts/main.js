0d340b4609c489712080103b2d2cf549891720b3
jozsef-sogor <44443711+jozsef-sogor@users.noreply.github.com>
2022-05-29 18:16:54 +0200

    Tabs javascript added

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,40 @@
 "use strict"
+
+//TABS
+const tabs = Array.from(document.querySelectorAll(".tabs__tab-title"))
+
+function resetActiveTabContents() {
+    tabs.forEach(tab => {
+        const tabId = tab.dataset.tabid;
+        const tabContent = document.querySelector(`#tab-${tabId}`)
+        tabContent.style.display = "none";
+    })
+}
+
+function resetActiveTabs() {
+    tabs.forEach(tab => {
+        tab.dataset.isactive = "false"
+    })
+}
+
+function changeActiveTab(tabId) {
+    const newActiveTab = document.querySelector(`[data-tabid="${tabId}"]`);
+    const newActiveTabContent = document.querySelector(`#tab-${tabId}`);
+
+    resetActiveTabs();
+    resetActiveTabContents();
+    console.log(newActiveTab)
+    console.log(newActiveTabContent)
+    newActiveTab.dataset.isactive = "true";
+    newActiveTabContent.style.display = "block";
+}
+
+tabs.forEach(tab => {
+    if(tab.dataset.isactive) {
+        const activeTabContentId = tab.dataset.tabid;
+        document.querySelector(`#tab-${activeTabContentId}`).style.display = "block";
+    }
+    tab.addEventListener("click", function() {
+        changeActiveTab(this.dataset.tabid)
+    })
+})
